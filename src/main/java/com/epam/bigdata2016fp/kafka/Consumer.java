fcfb1d349b61dde3112863b41677b80b23509655
ValeryYahorau <yehorav.valery@gmail.com>
2016-10-27 15:20:16 +0300

    Global refactoring.

--- src/main/java/com/epam/bigdata2016fp/kafka/Consumer.java
+++ src/main/java/com/epam/bigdata2016fp/kafka/Consumer.java
@@ -12,26 +12,26 @@ import java.util.Properties;
 
 public class Consumer {
     public static void main(String[] args) throws IOException {
-        KafkaConsumer<String, String> consumer;
-        try (InputStream props = Resources.getResource("consumer.props").openStream()) {
-            Properties properties = new Properties();
-            properties.load(props);
-            consumer = new KafkaConsumer<>(properties);
-        }
-        consumer.subscribe(Arrays.asList("logs-topic"));
-        int timeouts = 0;
-
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(200);
-            if (records.count() == 0) {
-                timeouts++;
-            } else {
-                System.out.printf("Got %d records after %d timeouts\n", records.count(), timeouts);
-                timeouts = 0;
-            }
-            for (ConsumerRecord<String, String> record : records) {
-                System.out.println(record.toString());
-            }
-        }
+//        KafkaConsumer<String, String> consumer;
+//        try (InputStream props = Resources.getResource("consumer.props").openStream()) {
+//            Properties properties = new Properties();
+//            properties.load(props);
+//            consumer = new KafkaConsumer<>(properties);
+//        }
+//        consumer.subscribe(Arrays.asList("logs-topic"));
+//        int timeouts = 0;
+//
+//        while (true) {
+//            ConsumerRecords<String, String> records = consumer.poll(200);
+//            if (records.count() == 0) {
+//                timeouts++;
+//            } else {
+//                System.out.printf("Got %d records after %d timeouts\n", records.count(), timeouts);
+//                timeouts = 0;
+//            }
+//            for (ConsumerRecord<String, String> record : records) {
+//                System.out.println(record.toString());
+//            }
+//        }
     }
 }
