d2d8d58c08acb55b79f0e5165924207e04a0f71f
ValeryYahorau <yehorav.valery@gmail.com>
2016-10-31 18:31:26 +0300

    Added category logic.

--- src/main/java/com/epam/bigdata2016fp/sparkstreaming/SparkStreamingApp.java
+++ src/main/java/com/epam/bigdata2016fp/sparkstreaming/SparkStreamingApp.java
@@ -38,50 +38,49 @@ public class SparkStreamingApp {
         Map<String, String> dict2 = DictionaryUtils.tagsDictionry(props.getHadoop());
         Broadcast<Map<String, String>> brTagsDict = jsc.sparkContext().broadcast(dict2);
 
-        LogisticRegressionModel model1 = LogisticRegressionModel.load(jsc.sparkContext().sc(), "tmp/fp/ml");
+        //LogisticRegressionModel model1 = LogisticRegressionModel.load(jsc.sparkContext().sc(), "tmp/fp/ml");
 
         JavaPairReceiverInputDStream<String, String> logs = KafkaProcessor.getStream(jsc, props.getKafkaConnection());
 
         //save to ELASTIC SEARCH
-        String index = props.getElasticSearch().getIndex();
-        String type = props.getElasticSearch().getType();
-        String confStr = index + "/" + type;
-        logs.map(keyValue -> {
-            ESModel model = ESModel.parseLine(keyValue._2());
-            CityInfo cityInfo = brCitiesDict.value().get(Integer.toString(model.getCity()));
-            model.setGeoPoint(cityInfo);
-            model.setMlResult(model1.predict(Vectors.dense(model.getOsName().hashCode(),
-                    model.getDevice().hashCode(),
-                    model.getUaFamily().hashCode(),
-                    model.getRegion(),
-                    model.getCity(),
-                    model.getDomain().hashCode(),
-                    model.getAddSlotWidth(),
-                    model.getAddSlotHeight(),
-                    model.getAddSlotVisability(),
-                    model.getAddSlotFormat(),
-                    brTagsDict.value().get(model.getUserTags()).split(",")[0].hashCode())));
-            return model;
-        })
-                .mapPartitions(HbaseProcessor::getUserCategory)
-                .map(ESModel::toStringifyJson).foreachRDD(jsonRdd -> JavaEsSpark.saveJsonToEs(jsonRdd, confStr));
+//        String index = props.getElasticSearch().getIndex();
+//        String type = props.getElasticSearch().getType();
+//        String confStr = index + "/" + type;
+//        logs.map(keyValue -> {
+//            ESModel model = ESModel.parseLine(keyValue._2());
+//            CityInfo cityInfo = brCitiesDict.value().get(Integer.toString(model.getCity()));
+//            model.setGeoPoint(cityInfo);
+//            model.setMlResult(model1.predict(Vectors.dense(model.getOsName().hashCode(),
+//                    model.getDevice().hashCode(),
+//                    model.getUaFamily().hashCode(),
+//                    model.getRegion(),
+//                    model.getCity(),
+//                    model.getDomain().hashCode(),
+//                    model.getAddSlotWidth(),
+//                    model.getAddSlotHeight(),
+//                    model.getAddSlotVisability(),
+//                    model.getAddSlotFormat(),
+//                    brTagsDict.value().get(model.getUserTags()).split(",")[0].hashCode())));
+//            return model;
+//        })
+//                .mapPartitions(HbaseProcessor::getUserCategory)
+//                .map(ESModel::toStringifyJson).foreachRDD(jsonRdd -> JavaEsSpark.saveJsonToEs(jsonRdd, confStr));
 
 
         //save to HBASE
-//        JavaDStream<LogLine> logLineStream = logs.map(keyValue -> LogLine.parseLogLine(keyValue._2()));
-//        logLineStream
-//                .filter(line -> !"null".equals(line.getiPinyouId()))
-//                .foreachRDD(rdd ->
-//                        rdd.map(line -> {
-//                            CityInfo cityInfo = brCitiesDict.value().get(Integer.toString(line.getCity()));
-//                            line.setGeoPoint(cityInfo);
-//                            String tags = brTagsDict.value().get(line.getUserTags());
-//                            line.setTagsList(tags);
-//                            Put put = LogLine.convertToPut(line, props.getHbase().getColumnFamily());
-//                            return put;
-//                        }).foreachPartition(iter -> HbaseProcessor.saveToTable(iter, props.getHbase()))
-//                );
-
+        JavaDStream<LogLine> logLineStream = logs.map(keyValue -> LogLine.parseLogLine(keyValue._2()));
+        logLineStream
+                .filter(line -> !"null".equals(line.getiPinyouId()))
+                .foreachRDD(rdd ->
+                        rdd.map(line -> {
+                            CityInfo cityInfo = brCitiesDict.value().get(Integer.toString(line.getCity()));
+                            line.setGeoPoint(cityInfo);
+                            String tags = brTagsDict.value().get(line.getUserTags());
+                            line.setTagsList(tags);
+                            Put put = LogLine.convertToPut(line, props.getHbase().getColumnFamily());
+                            return put;
+                        }).foreachPartition(iter -> HbaseProcessor.saveToTable(iter, props.getHbase()))
+                );
 
         jsc.start();
         jsc.awaitTermination();
